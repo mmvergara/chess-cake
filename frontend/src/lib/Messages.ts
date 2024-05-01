e0ef093fe8b88ed342d620aa4f5144eca875fd7a
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-01 15:35:39 +0800

    Add Messages.ts with game-related constants

---
+++ frontend/src/lib/Messages.ts
@@ -0,0 +1,6 @@
+export const START_QUEUE = "START_QUEUE";
+export const IN_QUEUE = "IN_QUEUE";
+export const GAME_STARTED = "GAME_STARTED";
+
+export const MOVE = "MOVE";
+export const MOVE_MADE = "MOVE_MADE";
