bd590bce128f3a7643e2a64c5cee6ab69fe55a0b
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-01 15:35:32 +0800

    Add new message constants and update existing ones

--- backend/src/Messages.ts
+++ backend/src/Messages.ts
@@ -1,2 +1,8 @@
-export const INITIALIZE_GAME = "INITIALIZE_GAME";
+export const START_QUEUE = "START_QUEUE";
+export const IN_QUEUE = "IN_QUEUE";
+export const GAME_STARTED = "GAME_STARTED";
+
 export const MOVE = "MOVE";
+export const MOVE_MADE = "MOVE_MADE";
+
+export const ERROR = "ERROR";
